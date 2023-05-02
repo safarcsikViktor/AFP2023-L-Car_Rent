fa91f34d212f5ba9fe3c18a19dc03bf0b6ad46a9
geno200 <83767448+geno200@users.noreply.github.com>
2023-05-02 15:06:33 +0200

    Update and rename insert_car_ca-fuel_t-transm-car_statissions- to insert_car_ca-fuel_t-transm-car_stat.sql

---
+++ CODE/insert_car_ca-fuel_t-transm-car_stat.sql
@@ -0,0 +1,12 @@
+INSERT INTO `car_category` (`car_category`, `car_category_description`) VALUES 
+('M1', ' Személyszállító gépkocsik, a gépjárművezető ülésén kívül legfeljebb nyolc ülőhellyel.'),
+('M2', 'Személyszállító gépkocsik, a gépjárművezető ülésén kívül több mint nyolc ülőhellyel és legfeljebb 5 t műszakilag megengedett legnagyobb össztömeggel.'),
+('M3', 'Személyszállító gépkocsik, a gépjárművezető ülésén kívül több mint nyolc ülőhellyel és több mint 5 t műszakilag megengedett legnagyobb össztömeggel.'), 
+('N1', 'Áruszállító gépkocsik, legfeljebb 3,5 t műszakilag megengedett legnagyobb össztömeggel.'), 
+('N2', 'Áruszállító gépkocsik, több mint 3,5 t, de legfeljebb 12 t műszakilag megengedett legnagyobb össztömeggel.'), 
+('N3', 'Áruszállító gépkocsik, több mint 12 t műszakilag megengedett legnagyobb össztömeggel.\r\n'), 
+('L1', 'Kétkerekű járművek legfeljebb 45 km/ó tervezési sebességgel amelyben a motor: - hengerűrtartalma legfeljebb 50 cm3 belső égésű motor esetében, vagy legnagyobb folyamatos névleges teljesítménye legfeljebb 4 kW elektromos motor esetében'), 
+('L2', 'Háromkerekű járművek, legfeljebb 45 km/ó tervezési sebességgel amelyben a motor:\r\n- hengerűrtartalma legfeljebb 50 cm3, ha szikragyújtású, vagy\r\n- legnagyobb hasznos teljesítménye legfeljebb 4 kW egyéb belső égésű motor esetében, vagy\r\n- legnagyobb folyamatos névleges teljesítménye legfeljebb 4 kW, elektromos motor esetében'), 
+('L3', 'Motorkerékpár 50 cm3 és 45 km/h felett'), 
+('L4', 'Motorkerékpár, oldalkocsis 50 cm3 és 45 km/h felett'), 
+('L5', 'háromkerekű tricikli, azaz három szimmetrikus elrendezésű kerékkel rendelkező jármű, amennyiben 50 cm3-nél nagyobb hengerűrtartalmú belső égésű motorral vannak ellátva és/vagy a tervezési sebességük nagyobb, mint 45 km/ó');
