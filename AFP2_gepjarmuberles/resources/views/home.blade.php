4ce43ef969181e94e13833e49d283fec53e10dd3
TimeaNyiri <1nyiri.timea@gmail.com>
2023-06-20 21:10:36 +0200

    főoldal

--- AFP2_gepjarmuberles/resources/views/home.blade.php
+++ AFP2_gepjarmuberles/resources/views/home.blade.php
@@ -1,1 +1,9 @@
-<h1>Autóbérlés</h1>
+@extends('layouts.app')
+
+@section('content')
+   <h1>Autóbérlés</h1>
+    <p>
+    Cégünk 10 éve foglalkozik autóbérléssel. A jelenleg elérhető autóinkat megtekintheti 
+    az fenti menükön.
+    </p>
+@endsection
