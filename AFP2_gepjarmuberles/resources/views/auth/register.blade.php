99caa066174ea6a0674b82e4fbeb58690333d905
geno200 <gvczinege13@gmail.com>
2023-06-26 21:28:32 +0200

    Update register.blade.php

--- AFP2_gepjarmuberles/resources/views/auth/register.blade.php
+++ AFP2_gepjarmuberles/resources/views/auth/register.blade.php
@@ -9,31 +9,31 @@
                 <form action="{{ route('auth.register') }}" method="POST">
                     @csrf
                     <div class="mb-3">
-                        <label class="mb-2" for="email">{{ __('Email cím') }}</label>
-                        <input class="form-control {{ $errors->has('email') ? ' is-invalid' : '' }}"
-                        type="text" name="email" value="{{ old('email') }}">
-                        @if ($errors->has('email'))
-                            <p class="invalid-feedback">{{ $errors->first('email') }}</p>
+                        <label class="mb-2" for="email_address">{{ __('Email cím') }}</label>
+                        <input class="form-control {{ $errors->has('email_address') ? ' is-invalid' : '' }}"
+                        type="text" name="email_address" value="{{ old('email_address') }}">
+                        @if ($errors->has('email_address'))
+                            <p class="invalid-feedback">{{ $errors->first('email_address') }}</p>
                         @endif
                     </div>
                     <div class="mb-3">
-                        <label class="mb-2" for="name">{{ __('Teljes név') }}</label>
-                        <input class="form-control {{ $errors->has('name') ? ' is-invalid' : '' }}"
-                        type="text" name="name" value="{{ old('name') }}">
-                        @if ($errors->has('name'))
-                            <p class="invalid-feedback">{{ $errors->first('name') }}</p>
+                        <label class="mb-2" for="user_name">{{ __('Felhasználónév') }}</label>
+                        <input class="form-control {{ $errors->has('user_name') ? ' is-invalid' : '' }}"
+                        type="text" name="user_name" value="{{ old('user_name') }}">
+                        @if ($errors->has('user_name'))
+                            <p class="invalid-feedback">{{ $errors->first('user_name') }}</p>
                         @endif
                     </div>
                     <div class="mb-3">
-                        <label class="mb-2" for="password">{{ __('Jelszó') }}</label>
-                        <input class="form-control {{ $errors->has('password') ? ' is-invalid' : '' }}"
-                        type="password" name="password" value="{{ old('password') }}">
-                        @if ($errors->has('password'))
-                            <p class="invalid-feedback">{{ $errors->first('password') }}</p>
+                        <label class="mb-2" for="pwd">{{ __('Jelszó') }}</label>
+                        <input class="form-control {{ $errors->has('pwd') ? ' is-invalid' : '' }}"
+                        type="password" name="pwd" value="{{ old('pwd') }}">
+                        @if ($errors->has('pwd'))
+                            <p class="invalid-feedback">{{ $errors->first('pwd') }}</p>
                         @endif
                     </div>
                     <div class="mb-3">
-                        <label class="mb-2" for="password_confirmation">{{ __('Jelszó megerősítése') }}</label>
+                        <label class="mb-2" for="password_confirmation">{{ __('Jelszó ismét') }}</label>
                         <input class="form-control {{ $errors->has('password_confirmation') ? ' is-invalid' : '' }}"
                         type="password" name="password_confirmation" value="{{ old('password_confirmation') }}">
                         @if ($errors->has('password_confirmation'))
