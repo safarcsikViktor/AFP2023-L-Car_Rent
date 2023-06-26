46c3ab82ba2086d5055c25989c44d44d66f1632c
geno200 <gvczinege13@gmail.com>
2023-06-26 21:17:02 +0200

    Update User.php

--- AFP2_gepjarmuberles/app/Models/User.php
+++ AFP2_gepjarmuberles/app/Models/User.php
@@ -18,9 +18,9 @@ class User extends Authenticatable
      * @var array<int, string>
      */
     protected $fillable = [
-        'name',
-        'email',
-        'password',
+        'user_name',
+        'email_address',
+        'pwd',
     ];
 
     /**
@@ -29,7 +29,7 @@ class User extends Authenticatable
      * @var array<int, string>
      */
     protected $hidden = [
-        'password',
+        'pwd',
         'remember_token',
     ];
 
