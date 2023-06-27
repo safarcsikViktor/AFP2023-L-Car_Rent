4c196b7c0b150491a5e4d4571a5441738e409eab
geno200 <gvczinege13@gmail.com>
2023-06-27 13:06:46 +0200

    Update home.blade.php

--- AFP2_gepjarmuberles/resources/views/home.blade.php
+++ AFP2_gepjarmuberles/resources/views/home.blade.php
@@ -15,24 +15,29 @@
     <div class="row">
         <div class="col-md-6">
             <div class="card car-card">
-                <img src="/kepek/audia4.png" alt="Audi A4" class="card-img-top">
+                <div class="ratio ratio-16x9">
+                    <img src="{{ asset('kepek/audia4.png') }}" alt="Audi A4" class="card-img-top">
+                </div>
                 <div class="card-body">
                     <h5 class="card-title">Audi A4</h5>
                     <p class="card-text">
-                        Az Audi A4 egy elegáns és stílusos autó, amely tökéletes választás minden alkalomra.
-                        Bérelje ki most és élvezze a vezetés élményét!
+                        Az Audi A4 elegáns autó, amely garantálja az exkluzív vezetési élményt.
+                        Kifinomult belső térrel és modern technológiával rendelkezik.
+                        Élvezze a dinamikus vezetést és a luxus érzését!
                     </p>
                 </div>
             </div>
         </div>
         <div class="col-md-6">
             <div class="card car-card">
-                <img src="/kepek/merci.png" alt="Mercedes-Benz C-Class" class="card-img-top">
+                <div class="ratio ratio-16x9">
+                    <img src="{{ asset('kepek/merci.png') }}" alt="Mercedes-Benz C-Class" class="card-img-top">
+                </div>
                 <div class="card-body">
                     <h5 class="card-title">Mercedes-Benz C-Class</h5>
                     <p class="card-text">
-                        A Mercedes-Benz C-Class luxusautója egyedülállóan kombinálja az eleganciát és a teljesítményt.
-                        Ne hagyja ki ezt a kivételes autót!
+                        A Mercedes-Benz luxusautója egyedülállóan kombinálja az eleganciát és a teljesítményt.
+                        Bérelje ki most és élvezze a vezetés élményét a magas színvonalú kényelemmel!
                     </p>
                 </div>
             </div>
